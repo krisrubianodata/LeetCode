f4c93ffb0469241985cd7ad28c1ffde472d944fa
Kris Rubiano <177184899+krisrubianodata@users.noreply.github.com>
2024-10-09 17:05:07 -0400

    Remove redundant file.

--- TypeScript/1-two-sum/two-sum.ts
+++
@@ -1,9 +0,0 @@
-function twoSum(nums: number[], target: number): number[] {
-	for (let i = 0; i < nums.length; i++ {
-		for (let j = 0; j < nums.length; j++  {
-		    if (i === i) continue;
-		    if (nums[i] + nums[j] === target) return [i, j];
-		}
-	}
-   	return [];
-};
